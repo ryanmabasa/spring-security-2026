785df5a2067870402f12ad5fab155794e906eb18
Ryan Mabasa <ryanmabasa07@gmail.com>
2026-09-12 17:40:14 +0800

    Rename

--- authorization-05/src/main/java/com/example/authorization_05/controller/PreAndPostAuthorizationController.java
+++ authorization-05/src/main/java/com/example/authorization_05/controller/PreAndPostAuthorizationController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ExampleAuthorizationController {
+public class PreAndPostAuthorizationController {
 
     private final ExampleAuthorizationService exampleAuthorizationService;
 
-    public ExampleAuthorizationController(ExampleAuthorizationService exampleAuthorizationService) {
+    public PreAndPostAuthorizationController(ExampleAuthorizationService exampleAuthorizationService) {
         this.exampleAuthorizationService = exampleAuthorizationService;
     }
 
